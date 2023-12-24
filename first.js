ed321f32f9d45059e607f9c112efe636df4dd247
Tamoor Ali <tamoorali35@gmail.com>
2023-12-24 20:23:42 +0500

    question 1 and 2 learned js object

---
+++ first.js
@@ -0,0 +1,25 @@
+// Q1 create a const object caled product to store information
+
+const product = {
+    name : "ball pen",
+    price: 30,
+    inStock : true,
+    rating : 4.5,
+    discount : 0.10,
+
+};
+
+
+// Q2 profile object to store information
+
+const profile = {
+    fullName : "Tamoor Khaskheli",
+    followers : 569,
+    following : 5,
+    post : 190,
+    slogan : "knowlodge is everything learning is success",
+    isOnline : false,
+
+};
+
+console.log(typeof profile.isOnline)
